9f12db814c6b45675f164c6306e9a148cd904bee
a.salahov <a.salahov@aisgorod.ru>
2022-10-04 09:50:42 +0400

    добавлен DAL для PostgreSQL и получение процедур для PostgreSQL (пока без использования в программе)

---
+++ CompareBases/DAL/DALPostgreSQL.cs
@@ -0,0 +1,142 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NpgsqlTypes;
+using System.Threading;
+
+
+namespace CompareBases.DAL
+{
+    class DALPostgreSQL
+	{
+		/// <summary>
+		/// Коннектион к базе Postgre
+		/// </summary>
+		private NpgsqlConnection Connection;
+
+		/// <summary>
+		/// Строка подключения к базе, обязательна к заполнению
+		/// </summary>
+		private string ConnectionString;
+
+		/// <summary>
+		/// Массив DALPostgreSQL для потоков
+		/// </summary>
+		private static Dictionary<int, DALPostgreSQL> Pool = new Dictionary<int, DALPostgreSQL>();
+
+		public static void SetConnectionString(string connectionString)
+		{
+			DALPostgreSQL dal = GetThreadDal();
+			if (dal.ConnectionString != connectionString)
+			{
+				if (dal.Connection != null)
+				{
+					dal.Connection.Close();
+					dal.Connection = null;
+				}
+				dal.ConnectionString = connectionString;
+			}
+		}
+
+		public static void CloseConnection()
+		{
+			DALPostgreSQL dal = GetThreadDal();
+			if (dal.Connection != null)
+			{
+				dal.Connection.Close();
+				dal.Connection = null;
+			}
+		}
+
+		public static DataSet ExecuteDataSet(string sql, NpgsqlParameter[] parameters)
+		{
+			using (var adapter = GetAdapter(sql, parameters))
+			{
+				var dt = new DataSet();
+				adapter.Fill(dt);
+				return dt;
+			}
+		}
+
+		public static DataTable ExecuteDataTable(string sql, NpgsqlParameter[] parameters)
+		{
+			var dt = new DataTable();
+			ExecuteDataTable(sql, parameters, dt);
+			return dt;
+		}
+
+		public static void ExecuteDataTable(string sql, NpgsqlParameter[] parameters, DataTable dt)
+		{
+			using (var adapter = GetAdapter(sql, parameters))
+			{
+				adapter.Fill(dt);
+			}
+		}
+
+		private void CheckConnection()
+		{
+			if (Connection == null)
+			{
+				Connection = new NpgsqlConnection(ConnectionString);
+			}
+			if (Connection.State == ConnectionState.Broken)
+			{
+				Connection.Close();
+			}
+			if (Connection.State == ConnectionState.Closed)
+			{
+				try
+				{
+					Connection = new NpgsqlConnection(ConnectionString);
+					Connection.Open();
+				}
+				catch (Exception ex)
+				{
+					if (Connection.State == ConnectionState.Open) 
+						Connection.Close();
+
+					throw new ApplicationException("Ошибка соединения с сервером.", ex);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Получает DAL для текущего потока
+		/// </summary>
+		private static DALPostgreSQL GetThreadDal()
+		{
+			int threadId = Thread.CurrentThread.ManagedThreadId;
+			DALPostgreSQL dal;
+			Pool.TryGetValue(threadId, out dal);
+			if (dal == null)
+			{
+				dal = new DALPostgreSQL();
+				Pool.Add(threadId, dal);
+			}
+			return dal;
+		}
+
+		private static NpgsqlDataAdapter GetAdapter(string sql, NpgsqlParameter[] parameters)
+		{
+			DALPostgreSQL dal = GetThreadDal();
+			dal.CheckConnection();
+
+			NpgsqlCommand sc = new NpgsqlCommand(sql, dal.Connection);
+			sc.CommandType = CommandType.Text;
+			sc.CommandTimeout = 0;
+			if (parameters != null) sc.Parameters.AddRange(parameters);
+			foreach (NpgsqlParameter parameter in sc.Parameters)
+			{
+				if (parameter.Value == null)
+					parameter.Value = DBNull.Value;
+				if (parameter.NpgsqlDbType == NpgsqlDbType.Timestamp && parameter.Value.Equals(new DateTime(1, 1, 1)))
+					parameter.Value = DBNull.Value;
+			}
+			return new NpgsqlDataAdapter(sc);
+		}
+	}
+}
---
+++ CompareBases/Model/SQLScriptDB_PgSql.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareBases
+{
+    public class SQLScriptDB_PgSql
+    {
+        public bool WithTable;
+        public bool TableWithTrigger;
+        public string FilterPrefix;
+        public List<string> FilterIgnoreByPrefix;
+        public List<string> FilterIgnoreByPostfix;
+
+        public string GetScriptCountObjects()
+        {
+            return GetScriptPrepareWhere() + @"
+select count(1)
+from sys.objects so 
+	inner join sys.schemas ss on so.schema_id = ss.schema_id
+	--left join sys.sql_modules sm on sm.object_id = so.object_id
+" + GetScriptWhere();
+        }
+
+        private string GetScriptSchemas(bool useFilterPrefix = true)
+        {
+            return @" 
+union all
+SELECT -1*ROW_NUMBER() over (order by schema_name) as object_id
+	, ' ' as type
+    , schema_name as ""schemaName""
+    , ' ' as name
+    , 'CREATE SCHEMA [' || schema_name || ']' as definition
+    , now() as create_date
+    , now() as modify_date
+FROM information_schema.schemata
+    "
+                + (!useFilterPrefix || string.IsNullOrWhiteSpace(FilterPrefix) ? "" : " where '[' || schema_name || ']' like '"
+                    + GetStringLike(FilterPrefix)
+                    + "%'");
+        }
+
+        private string GetScriptWhere(string types = null, bool useFilterPrefix = true)
+        {
+            return @"
+    left join filterIgnore fi on obj.name like fi.mask
+    where obj.schemaname not in ('pg_catalog','information_schema')
+        and fi.mask is null
+"
+                + (!useFilterPrefix || string.IsNullOrWhiteSpace(FilterPrefix) ? "" : " and obj.name like '"
+                    + GetStringLike(FilterPrefix)
+                    + "%'")
+                ;
+        }
+
+        private string GetStringLike(string orig)
+        {
+            return orig.Replace("'", "''").Replace("_", "[_]");
+        }
+
+        private string GetScriptPrepareWhere()
+        {
+            return @"
+DROP TABLE IF EXISTS filterIgnore;
+CREATE TEMPORARY TABLE filterIgnore(mask text);
+"
+                + FilterIgnoreByPrefix.Where(f => !string.IsNullOrWhiteSpace(f)).Aggregate(""
+                    , (s, f) => (s == "" ? "insert into filterIgnore(mask) " : s + Environment.NewLine + "union ")
+                        + "select '" + GetStringLike(f) + "%'")
+                + Environment.NewLine
+                + FilterIgnoreByPostfix.Where(f => !string.IsNullOrWhiteSpace(f)).Aggregate(""
+                    , (s, f) => (s == "" ? "insert into filterIgnore(mask) " : s + Environment.NewLine + "union ")
+                        + "select '%" + GetStringLike(f) + "'")
+                + Environment.NewLine;
+        }
+
+        public string GetScriptDefinitionObjects()
+        {
+            return GetScriptPrepareWhere() + @"
+--процедуры/функции
+select p.oid as object_id
+	, case when p.prokind = 'f' then 'FN' else 'P' end as type
+	, n.nspname as ""schemaName""
+    , p.proname as name
+    , pg_get_functiondef(p.oid) as definition
+    , now() as create_date
+    , now() as modify_date
+from pg_proc p
+left join pg_namespace n on n.oid = p.pronamespace
+cross join lateral (select '[' || n.nspname || '].[' || p.proname || ']' as name, n.nspname as schemaname) obj
+" + GetScriptWhere() + GetScriptSchemas();
+
+        }
+    }
+}
